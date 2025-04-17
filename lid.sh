a76dc574c431bf5e3c481caa507b92089c6d9b0e
Benjamin Ellis <benjaminjellis@protonmail.com>
2025-04-17 18:12:07 +0100

    fix: lid switch event

--- lid.sh
+++ lid.sh
@@ -2,16 +2,21 @@
 
 if hyprctl monitors | grep -q "DP-4"; then
   if [[ $1 == "open" ]]; then
+    echo 'monitor open w/ external'
     hyprctl keyword monitor "eDP-1,preferred,auto,1"
   else
+    echo 'monitro closed w/ external'
     hyprctl keyword monitor "eDP-1,disable"
   fi
 else
   if [[ $1 == "close" ]]; then
+
+    echo 'monitro closed w/o external'
     # no external monitor
     pidof hyprlock || hyprlock
     systemctl suspend
+  else
+    echo 'monitro open w/o external'
   fi
 
-  echo "No monitor"
 fi
