267f1f47946cc6561806fe7f088c55c006134501
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-04-17 22:45:15 +0100

    fix: keyboard layout and use vim keybinds for swapping focus

--- lid.sh
+++ lid.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
 if hyprctl monitors | grep -q "DP-4"; then
+  # found external monitor
   if [[ $1 == "open" ]]; then
-    echo 'monitor open w/ external'
+    # lid is open
     hyprctl keyword monitor "eDP-1,preferred,auto,1"
   else
-    echo 'monitro closed w/ external'
+    # lid is closed
     hyprctl keyword monitor "eDP-1,disable"
   fi
 else
+  # did not find external monitor
   if [[ $1 == "close" ]]; then
-
-    echo 'monitro closed w/o external'
-    # no external monitor
+    # lid is closed
     pidof hyprlock || hyprlock
     systemctl suspend
-  else
-    echo 'monitro open w/o external'
   fi
 
 fi
