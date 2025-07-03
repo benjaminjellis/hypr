fa2236f811bb6a3a2e3fada47ebd4b62d98a103f
Benjamin Ellis <benjaminjellis@protomail.com>
2025-07-03 23:55:53 +0100

    feat: tweaks

--- lid.sh
+++ lid.sh
@@ -5,7 +5,7 @@ if hyprctl monitors | grep -q "DP-6"; then
   # found external monitor
   if [[ $1 == "open" ]]; then
     # lid is open
-    hyprctl keyword monitor "eDP-1,preferred,auto,1"
+    hyprctl keyword monitor "eDP-1,preferred,auto,1.2"
   else
     # lid is closed
     hyprctl keyword monitor "eDP-1,disable"
