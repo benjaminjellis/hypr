75d947154d27041dfb55a5c47ada4d0965f4c6d9
benjaminjellis <benjaminjellis@protonmail.com>
2025-07-19 16:11:58 +0100

    feat: update

--- lid.sh
+++ lid.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-if hyprctl monitors | grep -q "DP-7" || hyprctl monitors | grep -q "DP-6"; then
+if [ -z "$1" ]; then
+  if grep -q "open" /proc/acpi/button/lid/LID0/state; then
+    LID_STATUS="open"
+  else
+    LID_STATUS="close"
+  fi
+else
+  LID_STATUS="$1"
+fi
 
-  # found external monitor
-  if [[ $1 == "open" ]]; then
-    # lid is open
+if hyprctl monitors | grep -q "DP-7" || hyprctl monitors | grep -q "DP-6"; then
+  if [[ $LID_STATUS == "open" ]]; then
     hyprctl keyword monitor "eDP-1,preferred,auto,1.333"
   else
-    # lid is closed
     hyprctl keyword monitor "eDP-1,disable"
   fi
 else
-  # did not find external monitor
-  if [[ $1 == "close" ]]; then
-    # lid is closed
+  if [[ $LID_STATUS == "close" ]]; then
     systemctl suspend
   fi
-
 fi
